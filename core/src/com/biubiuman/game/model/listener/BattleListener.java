554e1eb2743197176e3e7aff03e55a61e30a4f4f
Kimitor <15651125909@163.com>
2016-03-22 22:11:38 +0800

    add the interface listener

---
+++ core/src/com/biubiuman/game/model/listener/BattleListener.java
@@ -0,0 +1,15 @@
+package com.biubiuman.game.model.listener;
+
+public interface BattleListener {
+
+	
+	public abstract void onDeadFunction();
+	
+	public abstract void onWinFunction();
+	
+	public abstract void onHurtFunction();
+	
+	public abstract void onHitFunction();
+	
+	
+}
